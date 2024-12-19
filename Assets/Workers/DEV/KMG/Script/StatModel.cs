86ac885a7675995a34b9b310e190e78b248e09ef
februarywind <89524114+februarywind@users.noreply.github.com>
2024-12-19 18:02:12 +0900

    [Feat] 스탯 모델 제작

---
+++ Assets/Workers/DEV/KMG/Script/StatModel.cs
@@ -0,0 +1,50 @@
+using System;
+using UnityEngine;
+public enum AdditionAbility
+{
+    AllPower, AllPowerPer, SkillPowerPer, ElementalPowerPer, CriticalDamage, Critical, MaxHp, MaxHpPer, MaxStamina, MaxStaminaPer, StaminarEgeneration, MpGain, MoveSpeed, MaxObject, ChipGetAmount, Size
+}
+
+public class StatModel : MonoBehaviour
+{
+    [Header("실시간 능력치")]
+    [SerializeField] float currentHp;
+    public float CurrentHp { get => currentHp; set { currentHp = value; OnCurHpChange.Invoke(value); } }
+
+    [SerializeField] float currentMp;
+    public float CurrentMp { get => currentMp; set { currentMp = value; OnCurMpChange.Invoke(value); } }
+
+    [SerializeField] float currentStamina;
+    public float CurrentStamina { get => currentStamina; set { currentStamina = value; OnCurStaminaChange.Invoke(value); } }
+
+    [SerializeField] float chip;
+    public float Chip { get => chip; set { chip = value; OnChipChange.Invoke(value); } }
+
+    [SerializeField] float blackChip;
+    public float BlackChip { get => blackChip; set { blackChip = value; OnBlackChipChange.Invoke(value); } }
+
+    [Header("추가 능력치")] // 아이템으로 상승하는 능력치 편의상 배열로 만들었음
+    [SerializeField] float[] additionAbility = new float[(int)AdditionAbility.Size];
+    public float GetAbility(AdditionAbility ability)
+    {
+        return additionAbility[(int)ability];
+    }
+    public void SetAbility(AdditionAbility ability, float value)
+    {
+        additionAbility[(int)ability] += value;
+        OnStatChange.Invoke();
+    }
+
+    // 현재 체력 변경
+    public event Action<float> OnCurHpChange;
+    // 현재 마나 변경
+    public event Action<float> OnCurMpChange;
+    // 현재 스테미나 변경
+    public event Action<float> OnCurStaminaChange;
+    // 데이터 칩 변경
+    public event Action<float> OnChipChange;
+    // 블랙 데이터 칩 변경
+    public event Action<float> OnBlackChipChange;
+    // 능력치 변경
+    public event Action OnStatChange;
+}

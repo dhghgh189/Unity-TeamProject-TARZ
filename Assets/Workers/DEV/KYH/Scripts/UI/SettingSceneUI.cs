13ffb02abd0f32e04523b202500049d06e59dc61
KimYonai <msb0714@gmail.com>
2024-12-20 19:47:27 +0900

    [Refactor] 코드 설명 주석 추가
    스크립트에 코드 리뷰를 위한 설명 주석을 추가했습니다.

--- Assets/Workers/DEV/KYH/Scripts/UI/SettingSceneUI.cs
+++ Assets/Workers/DEV/KYH/Scripts/UI/SettingSceneUI.cs
@@ -6,95 +6,122 @@ using UnityEngine.UI;
 
 public class SettingSceneUI : MonoBehaviour
 {
-    [SerializeField] private GameObject titlePanel;
-    [SerializeField] private GameObject activeCPanel;
+    [SerializeField] private GameObject titlePanel;         // 타이틀 패널
+    [SerializeField] private GameObject activeCPanel;       // 현재 활성화 중인 패널
 
     [Header("<color=yellow>Input Manager</color>")]
-    [SerializeField] private ChangeInput inputManager;
+    [SerializeField] private ChangeInput inputManager;      // UI 네비게이션 InputManager 참조용
 
     [Header("<color=orange>Category Buttons</color>")]
-    [SerializeField] private Button gameplayButton;
-    [SerializeField] private Button langueButton;
-    [SerializeField] private Button soundButton;
-    [SerializeField] private Button keySettingsButton;
+    [SerializeField] private Button gameplayButton;         // 게임 플레이 카테고리 버튼
+    [SerializeField] private Button langueButton;           // 언어 카테고리 버튼
+    [SerializeField] private Button soundButton;            // 사운드 카테고리 버튼
+    [SerializeField] private Button keySettingsButton;      // 키 설정 카테고리 버튼
 
     [Header("<color=purple>Category Panels</color>")]
-    [SerializeField] private GameObject nonSelectPanel;
-    [SerializeField] private GameObject gameplayPanel;
-    [SerializeField] private GameObject languagePanel;
-    [SerializeField] private GameObject soundPanel;
+    [SerializeField] private GameObject nonSelectPanel;     // 설정 패널 첫 활성화 때 나오는 빈 패널
+    [SerializeField] private GameObject gameplayPanel;      // 게임 플레이 카테고리 패널
+    [SerializeField] private GameObject languagePanel;      // 언어 카테고리 패널
+    [SerializeField] private GameObject soundPanel;         // 사운드 카테고리 패널
+    [SerializeField] private GameObject keySettingsPanel;   // 키 설정 카테고리 패널
 
     [Header("<color=blue>Selected UI</color>")]
-    [SerializeField] private Toggle activeMinimapToggle;
-    //[SerializeField] private Dropdown languageDropdown;
-    [SerializeField] private Slider masterVolumeSlider;
+    [SerializeField] private Toggle activeMinimapToggle;    // 미니맵 활성화 체크 토글
+    //[SerializeField] private Dropdown languageDropdown;   // 언어 선택 드롭다운 (참조가 되지 않는 오류로 주석처리)
+    [SerializeField] private Slider masterVolumeSlider;     // 마스터 볼륨 조절 슬라이더
 
     private void Start()
     {
-        activeCPanel = nonSelectPanel;
-        inputManager.firstInput = gameplayButton;
-        inputManager.firstInput.Select();
+        activeCPanel = nonSelectPanel;                  // 현재 활성화 중인 패널을 nonSelectPanel로 설정
+        inputManager.firstInput = gameplayButton;       // 설정 패널의 UI 네비게이션 첫 Input을 gameplayButton로 설정
+        inputManager.firstInput.Select();               // 첫 Input으로 지정한 오브젝트를 선택 처리
     }
 
     private void OnEnable()
     {
-        activeCPanel = nonSelectPanel;
-        inputManager.firstInput = gameplayButton;
-        inputManager.firstInput.Select();
+        activeCPanel = nonSelectPanel;                  // 현재 활성화 중인 패널을 nonSelectPanel로 설정
+        inputManager.firstInput = gameplayButton;       // 설정 패널의 UI 네비게이션 첫 Input을 gameplayButton로 설정
+        inputManager.firstInput.Select();               // 첫 Input으로 지정한 오브젝트를 선택 처리
     }
 
     private void Update()
     {
+        // ESC(컨트롤러 B버튼) 입력 시 행동
         if (Input.GetKeyDown(KeyCode.Escape) || Input.GetKeyDown(KeyCode.Joystick1Button1))
         {
+            // 현재 활성화 중인 카테고리 패널에 맞는 카테고리 버튼 선택
             if (activeCPanel == gameplayPanel)
             {
                 gameplayButton.Select();
             }
-            else if (activeCPanel = languagePanel)
+            else if (activeCPanel == languagePanel)
             {
                 langueButton.Select();
             }
-            else if (activeCPanel = soundPanel)
+            else if (activeCPanel == soundPanel)
             {
                 soundButton.Select();
+            }
+            else if (activeCPanel == keySettingsPanel)
+            {
+                keySettingsButton.Select();
             }
         }
     }
 
+    // 게임 플레이 카테고리 버튼 클릭
     public void OnClickGamePlayButton()
     {
-        activeCPanel = gameplayPanel;
-        nonSelectPanel.SetActive(false);
+        activeCPanel = gameplayPanel;           // 현재 활성화 중인 패널을 게임 플레이 카테고리 패널로 설정
+        nonSelectPanel.SetActive(false);        // 게임 플레이 카테고리 패널을 제외하고 모두 비활성화
         gameplayPanel.SetActive(true);
         languagePanel.SetActive(false);
         soundPanel.SetActive(false);
-        activeMinimapToggle.Select();
+        keySettingsPanel.SetActive(false);
+        activeMinimapToggle.Select();           // activeMinimapToggle 오브젝트를 UI 네비게이션 Input 시작으로 선택
     }
 
+    // 언어 카테고리 버튼 클릭
+    // 언어 카테고리 버튼 클릭 기능에서 드롭다운 UI의 참조 오류가 있어 현재는 제대로 기능하지 않음.
     public void OnClickLanguageButton()
     {
-        activeCPanel = languagePanel;
-        nonSelectPanel.SetActive(false);
+        activeCPanel = languagePanel;           // 현재 활성화 중인 패널을 언어 카테고리 패널로 설정
+        nonSelectPanel.SetActive(false);        // 언어 카테고리 패널을 제외하고 모두 비활성화
         gameplayPanel.SetActive(false);
         languagePanel.SetActive(true);
         soundPanel.SetActive(false);
-        //languageDropdown.Select();
+        keySettingsPanel.SetActive(false);
+        //languageDropdown.Select();            // languageDropdown 오브젝트를 UI 네비게이션 Input 시작으로 선택
     }
 
+    // 사운드 카테고리 버튼 클릭
     public void OnClickSoundButton()
     {
-        activeCPanel = soundPanel;
-        nonSelectPanel.SetActive(false);
+        activeCPanel = soundPanel;              // 현재 활성화 중인 패널을 사운드 카테고리 패널로 설정
+        nonSelectPanel.SetActive(false);        // 사운드 카테고리 패널을 제외하고 모두 비활성화
         gameplayPanel.SetActive(false);
         languagePanel.SetActive(false);
         soundPanel.SetActive(true);
-        masterVolumeSlider.Select();
+        keySettingsPanel.SetActive(false);
+        masterVolumeSlider.Select();            // masterVolumeSlider 오브젝트를 UI 네비게이션 Input 시작으로 선택
     }
 
+    // 키 설정 카테고리 버튼 클릭
+    public void OnClickKeySettingsButton()
+    {
+        activeCPanel = soundPanel;              // 현재 활성화 중인 패널을 키 설정 카테고리 패널로 설정
+        nonSelectPanel.SetActive(false);        // 키 설정 카테고리 패널을 제외하고 모두 비활성화
+        gameplayPanel.SetActive(false);
+        languagePanel.SetActive(false);
+        soundPanel.SetActive(false);
+        keySettingsPanel.SetActive(true);
+        // TODO : 키세팅 패널 시작 UI Select
+    }
+
+    // 타이틀 화면으로 돌아가기 버튼 클릭
     public void OnClickBackToTitleButton()
     {
-        gameObject.SetActive(false);
-        titlePanel.SetActive(true);
+        gameObject.SetActive(false);            // 설정 패널 비활성화
+        titlePanel.SetActive(true);             // 타이틀 패널 활성화
     }
 }
--- Assets/Workers/DEV/KYH/Scripts/UI/TitleSceneUI.cs
+++ Assets/Workers/DEV/KYH/Scripts/UI/TitleSceneUI.cs
@@ -7,52 +7,58 @@ using UnityEngine.UI;
 
 public class TitleSceneUI : MonoBehaviour
 {
-    [SerializeField] private Button titleButton;
+    [SerializeField] private Button newGameButton;              // 타이틀 패널 시작 UI
 
     [Header("<color=yellow>Input Manager</color>")]
-    [SerializeField] private ChangeInput inputManager;
+    [SerializeField] private ChangeInput inputManager;          // UI 네비게이션 InputManager 참조용
 
     [Header("<color=orange>Load Game</color>")]
-    [SerializeField] private GameObject loadGamePanel;
-    [SerializeField] private LoadSceneUI loadScene;
+    [SerializeField] private GameObject loadGamePanel;          // 저장된 게임 불러오기 패널
+    [SerializeField] private LoadSceneUI loadScene;             // 저장된 게임 불러오기 패널 클래스
 
     [Header("<color=green>Settings</color>")]
-    [SerializeField] private GameObject settingsPanel;
-    [SerializeField] private SettingSceneUI settingScene;
+    [SerializeField] private GameObject settingsPanel;          // 설정 패널
+    [SerializeField] private SettingSceneUI settingScene;       // 설정 패널 클래스
 
     private void Start()
     {
-        inputManager.firstInput = titleButton;
-        inputManager.firstInput.Select();
+        inputManager.firstInput = newGameButton;  // 타이틀 패널의 UI 네비게이션 첫 Input을 newGameButton로 설정
+        inputManager.firstInput.Select();         // 첫 Input으로 지정한 오브젝트를 선택 처리
     }
 
     private void OnEnable()
     {
-        inputManager.firstInput = titleButton;
-        inputManager.firstInput.Select();
+        inputManager.firstInput = newGameButton;  // 타이틀 패널의 UI 네비게이션 첫 Input을 newGameButton로 설정
+        inputManager.firstInput.Select();         // 첫 Input으로 지정한 오브젝트를 선택 처리
     }
 
+    // 새로 시작 버튼 클릭
     public void OnClickNewGameButton()
     {
         SceneManager.LoadScene("GameScene");    // 바로 새 게임 씬으로 이동(게임 씬 이름 변경 필요)
     }
 
+    // 저장된 게임 시작 버튼 클릭
     public void OnClickLoadGameButton()
     {
-        gameObject.SetActive(false);
-        loadGamePanel.SetActive(true);
+        gameObject.SetActive(false);    // 타이틀 패널 비활성화
+        loadGamePanel.SetActive(true);  // 저장된 게임 불러오기 패널 활성화
     }
 
+    // 설정 버튼 클릭
     public void OnClickSettingsButton()
     {
-        gameObject.SetActive(false);
-        settingsPanel.SetActive(true);
+        gameObject.SetActive(false);    // 타이틀 패널 비활성화
+        settingsPanel.SetActive(true);  // 설정 패널 활성화
     }
 
+    // 게임 종료 버튼 클릭
     public void OnClickQuitGameButton()
     {
+        // Unity Editor일 경우, 에디터 플레이 모드 종료
         #if UNITY_EDITOR
                 UnityEditor.EditorApplication.isPlaying = false;
+        // Unity Editor가 아닐 경우, 어플리케이션 종료
         #else
                 Application.Quit();
         #endif

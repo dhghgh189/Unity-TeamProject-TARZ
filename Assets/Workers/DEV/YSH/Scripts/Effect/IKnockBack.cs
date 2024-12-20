2df07bda198a6195c6f768e53530db1f79a0cbaf
dhghgh189 <dhghgh189@gmail.com>
2024-12-20 22:09:33 +0900

    [Refactor] 요청받은 Summary 작성 및 필요없는 코드 제거

--- Assets/Workers/DEV/YSH/Scripts/Effect/IKnockBack.cs
+++ Assets/Workers/DEV/YSH/Scripts/Effect/IKnockBack.cs
@@ -2,6 +2,10 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+/// <summary>
+/// KnockBack 이펙트를 구현하기 위한 인터페이스
+/// 해당 인터페이스를 상속받아 실제 동작을 구현
+/// </summary>
 public interface IKnockBack
 {
     void KnockBack(GameObject attacker);

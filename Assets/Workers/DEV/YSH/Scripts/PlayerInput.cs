65e82adec9e8092e8ed16cdb461c249a6fdbe802
dhghgh189 <dhghgh189@gmail.com>
2024-12-20 20:35:01 +0900

    [Fix] 근접 공격 키 변경

--- Assets/Workers/DEV/YSH/Scripts/PlayerInput.cs
+++ Assets/Workers/DEV/YSH/Scripts/PlayerInput.cs
@@ -16,11 +16,11 @@ public class PlayerInput : MonoBehaviour
         InputDir = new Vector3(
             Input.GetAxisRaw("Horizontal"), 0, Input.GetAxisRaw("Vertical"));
 
-        // 추후 키 변경 필요
+        // 패드 지원되도록 구성해야 함
         TryDash = Input.GetKeyDown(KeyCode.LeftShift);
         TryJump = Input.GetKeyDown(KeyCode.Space);
         TryThrow = Input.GetMouseButtonDown(0);
-        TryMelee = Input.GetMouseButtonDown(1);
+        TryMelee = Input.GetKeyDown(KeyCode.F);
         TryDrain = Input.GetKey(KeyCode.LeftControl);
     }
 }

df0f5417c49ac26839a74356a00f556879af07d4
dhghgh189 <dhghgh189@gmail.com>
2024-12-22 16:35:55 +0900

    [Fix] 대쉬 로직 변경
    - 기존 카메라 정면 방향으로만 대쉬했지만 대쉬 진입시 방향 입력을 통해 원하는 방향으로 가능하도록 수정
    - 대쉬 진입 시 방향키를 누르면 카메라 정면 기준 입력한 방향으로 대쉬한다.

--- Assets/Workers/DEV/YSH/Scripts/State/Player/DashState.cs
+++ Assets/Workers/DEV/YSH/Scripts/State/Player/DashState.cs
@@ -25,8 +25,21 @@ public class DashState : BaseState<PlayerController>
 
         dashTimer = owner.Stat.DashTime;
 
-        // 카메라 정면을 바라본다.
-        owner.Movement.LookAt(camTrf.forward);
+        // 대쉬 시 입력값이 있는지 확인하기 위해 저장
+        moveDir = owner.PInput.InputDir.normalized;
+
+        // 방향키 입력이 있는 경우 카메라 정면을 기준으로 입력한 방향을 바라본다.
+        if (moveDir != Vector3.zero)
+        {
+            owner.Movement.LookAt(moveDir);
+        }
+        // 방향키 입력이 없는 경우 카메라 정면을 바라본다.
+        else
+        {
+            moveDir = Vector3.forward;
+            owner.Movement.LookAt(camTrf.forward);
+        }
+
         owner.Anim.CrossFade(Define.HASH_ANIM_DASH, 0.1f);
     }
 
@@ -42,7 +55,8 @@ public class DashState : BaseState<PlayerController>
             return;
         }
 
-        owner.Movement.Move(Vector3.forward * owner.Stat.DashSpeed);
+        // 지정된 방향으로 대쉬 진행
+        owner.Movement.Move(moveDir * owner.Stat.DashSpeed);
     }
 
     public override void OnExit()

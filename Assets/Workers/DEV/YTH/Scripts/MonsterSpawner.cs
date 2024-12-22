cf33354e46f5f8e54ebda78ae03c7c413f151ccb
TaeHeon Ryoo <taroni55@naver.com>
2024-12-23 01:57:51 +0900

    [Feat] 몬스터 스포너 Trigger 기반 수정

--- Assets/Workers/DEV/YTH/Scripts/MonsterSpawner.cs
+++ Assets/Workers/DEV/YTH/Scripts/MonsterSpawner.cs
@@ -1,18 +1,21 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class MonsterSpawner : MonoBehaviour
 {
     [SerializeField] ObjectPool _monsterPool;
 
+    [SerializeField] Collider[] _spawnTriggers;
+
     [SerializeField] Transform _spawnPoint;
 
-    private void Update()
+    private void Start()
     {
-        if (Input.GetKeyDown(KeyCode.P))
+        foreach (Collider spawnTrigger in _spawnTriggers)
         {
-            Spawn();
+           spawnTrigger.gameObject.AddComponent<SpawnTrigger>();
         }
     }
 
@@ -20,4 +23,6 @@ public class MonsterSpawner : MonoBehaviour
     {
         PooledObject instance = _monsterPool.GetPool(_spawnPoint.position, _spawnPoint.rotation);
     }
+
+   
 }

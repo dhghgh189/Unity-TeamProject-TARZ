6fb0e0499bd59aa5e40ce81ce08b2da168455628
scv829 <scv829@naver.com>
2024-12-24 21:08:19 +0900

    Test: 테스트 환경 수정
    테스트 환경 수정

--- Assets/Workers/DEV/HJS/Scripts/Test_SkillTable.cs
+++ Assets/Workers/DEV/HJS/Scripts/Test_SkillTable.cs
@@ -8,17 +8,23 @@ public class Test_SkillTable : MonoBehaviour
     [SerializeField] List<Test_SkillSlot> slots;
     [SerializeField] bool isRandom;
     [Header("Skill_List")]
-    [SerializeField] List<BaseSkillSO> _skills;
+    [SerializeField] List<BaseSkillSO> _skillList;
+    [SerializeField] Queue<BaseSkillSO> _skills;
     [Header("Skill_Handler")]
     [SerializeField] PlayerSkillHandler _handler;
 
-
-
     private void Start()
     {
+        _skills = new Queue<BaseSkillSO>();
+
         foreach(Test_SkillSlot slot in slots)
         {
             slot.playerSkillHandler = _handler;
+        }
+
+        foreach (BaseSkillSO skill in _skillList)
+        {
+            _skills.Enqueue(skill);
         }
     }
 
@@ -29,7 +35,9 @@ public class Test_SkillTable : MonoBehaviour
         // 2. 스킬 핸들러에게 장착 요청하기
         if (CheckEmpty(out int index))
         {
-            BaseSkillSO newSkill = (isRandom) ? Instantiate(_skills[Random.Range(0, _skills.Count)]) : Instantiate(_skills[index]);
+            //BaseSkillSO newSkill = (isRandom) ? Instantiate(_skills[Random.Range(0, _skills.Count)]) : Instantiate(_skills[index]);
+            BaseSkillSO newSkill = Instantiate(_skills.Dequeue());
+            _skills.Enqueue(newSkill);
             slots[index].SetSlot(newSkill);
             _handler.AddSkill(newSkill); 
         }

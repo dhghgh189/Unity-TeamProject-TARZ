f43b2f7dfd6b067db155a466ea13710e6410f9b9
TaeHeon Ryoo <taroni55@naver.com>
2024-12-24 09:46:56 +0900

    [Feat] 몬스터 피격 시 넉백 적용

--- Assets/Workers/DEV/YTH/Scripts/Spawner/PooledObject.cs
+++ Assets/Workers/DEV/YTH/Scripts/Spawner/PooledObject.cs
@@ -1,4 +1,6 @@
 using System;
+using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
 
 /// <summary>
@@ -69,6 +71,22 @@ public class PooledObject : MonoBehaviour, IKnockBack
 
     public void KnockBack(GameObject attacker)
     {
-       
+        StartCoroutine(KnockBackRoutine(attacker));
+    }
+
+    IEnumerator KnockBackRoutine(GameObject attacker)
+    {
+        float knockBackTime = 0.1f;
+        Vector3 moveDir = (transform.position - attacker.transform.position).normalized;
+        moveDir.y = 0;
+        while (true)
+        {
+            knockBackTime -= Time.deltaTime;
+            if (knockBackTime <= 0)
+                yield break;
+
+            transform.position += moveDir * 5f * Time.deltaTime;
+            yield return null;
+        }
     }
 }

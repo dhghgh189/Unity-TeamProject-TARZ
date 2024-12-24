6e08c4fc26b46104c9872814df75a9ac22de874c
dhghgh189 <dhghgh189@gmail.com>
2024-12-24 14:30:00 +0900

    [Fix] 플레이어 이동 애니메이션 이름 수정
    - Run -> Move로 수정

--- Assets/Workers/DEV/YSH/Scripts/Define.cs
+++ Assets/Workers/DEV/YSH/Scripts/Define.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 public static class Define
 {
     public static readonly int HASH_ANIM_IDLE = Animator.StringToHash("Idle");
-    public static readonly int HASH_ANIM_RUN = Animator.StringToHash("Run");
+    public static readonly int HASH_ANIM_MOVE = Animator.StringToHash("Move");
     public static readonly int HASH_ANIM_JUMP = Animator.StringToHash("Jump");
     public static readonly int HASH_ANIM_MELEE = Animator.StringToHash("Melee");
     public static readonly int HASH_ANIM_DASH = Animator.StringToHash("Dash");

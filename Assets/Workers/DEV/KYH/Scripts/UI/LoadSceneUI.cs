cc99d8ce592840f31d9d809e51c8dac5ef2c9d0d
KimYonai <msb0714@gmail.com>
2024-12-24 20:13:49 +0900

    [Set] 미니맵에서 몬스터 표시용 마크 설정
    미니맵에서 몬스터 표시용 마크를 설정했습니다.

--- Assets/Workers/DEV/KYH/Scripts/UI/LoadSceneUI.cs
+++ Assets/Workers/DEV/KYH/Scripts/UI/LoadSceneUI.cs
@@ -1,24 +1,21 @@
 using System.Collections;
+using System.IO;
 using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
+using Zenject;
 
 public class LoadSceneUI : MonoBehaviour
 {
-    [SerializeField] private TMP_Text gameText;
+    [Inject] SaveManager saveManager;
 
-    [SerializeField] private Button loadGame_01;
-    public Button LoadGame_01 { get; private set;}
-
-    // Start is called before the first frame update
-    void Start()
+    private void Awake()
     {
         
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Start()
     {
         
     }

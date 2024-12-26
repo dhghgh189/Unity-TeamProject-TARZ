e82fb0d8b5006942731a2dd11a9fd9b6d11b0ef6
februarywind <89524114+februarywind@users.noreply.github.com>
2024-12-26 19:01:14 +0900

    [Fix] 스탯 모델 뷰에서 공격력 삭제 및 블랙 데이터 칩 추가

--- Assets/Workers/DEV/KMG/Script/UI_StatModel.cs
+++ Assets/Workers/DEV/KMG/Script/UI_StatModel.cs
@@ -10,7 +10,7 @@ public class UI_StatModel : MonoBehaviour
     [SerializeField] private TMP_Text mpText;
     [SerializeField] private TMP_Text staminaText;
     [SerializeField] private TMP_Text chipText;
-    [SerializeField] private TMP_Text atackPowerText;
+    [SerializeField] private TMP_Text blackChipText;
     private void Awake()
     {
         statModel.OnStatChange += StatModel_OnStatChange;
@@ -18,35 +18,41 @@ public class UI_StatModel : MonoBehaviour
         statModel.OnCurMpChange += StatModel_OnCurMpChange;
         statModel.OnCurStaminaChange += StatModel_OnCurStaminaChange;
         statModel.OnChipChange += StatModel_OnChipChange;
+        statModel.OnBlackChipChange += StatModel_OnBlackChipChange;
 
         hpText.text = $"체력: {statModel.CurrentHp}/{statModel.MaxHp}";
         mpText.text = $"마나: {statModel.CurrentMp}";
         staminaText.text = $"스테미나: {statModel.CurrentStamina}/{statModel.MaxStamina}";
-        atackPowerText.text = $"공격력: {statModel.AllPowerPer}";
         chipText.text = $"데이터 칩: {statModel.Chip}";
+        blackChipText.text = $"블랙 데이터 칩: {statModel.BlackChip}";
 
         // UI 갱신을 위한 의미있는 함수
         statModel.SetAbility(AdditionAbility.AllPowerPer, 0);
     }
 
-    private void StatModel_OnCurHpChange(float cureentHp)
+    private void StatModel_OnCurHpChange(float currentHp)
     {
-        hpText.text = $"체력: {cureentHp}/{statModel.MaxHp}";
+        hpText.text = $"체력: {currentHp}/{statModel.MaxHp}";
     }
 
-    private void StatModel_OnCurMpChange(float cureentMp)
+    private void StatModel_OnCurMpChange(float curreentMp)
     {
-        mpText.text = $"마나: {cureentMp}";
+        mpText.text = $"마나: {curreentMp}";
     }
 
-    private void StatModel_OnCurStaminaChange(float cureentStamina)
+    private void StatModel_OnCurStaminaChange(float currentStamina)
     {
-        staminaText.text = $"스테미나: {cureentStamina}/{statModel.MaxStamina}";
+        staminaText.text = $"스테미나: {currentStamina}/{statModel.MaxStamina}";
     }
 
     private void StatModel_OnChipChange(float chip)
     {
         chipText.text = $"데이터 칩: {chip}";
+    }
+
+    private void StatModel_OnBlackChipChange(float blackChip)
+    {
+        blackChipText.text = $"블랙 데이터 칩: {blackChip}";
     }
 
     private void StatModel_OnStatChange()
@@ -61,10 +67,9 @@ public class UI_StatModel : MonoBehaviour
             }
         }
 
-        // 얍샙이
+        // UI 갱신을 위한
         statModel.CurrentHp += 0;
         statModel.CurrentMp += 0;
         statModel.ChangeStamina(0);
-        atackPowerText.text = $"공격력: {statModel.AllPowerPer}";
     }
 }

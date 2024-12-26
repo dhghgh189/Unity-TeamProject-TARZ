6d87487514f9c55f44ee27f6beb95051f5e8bb34
KimYonai <msb0714@gmail.com>
2024-12-26 12:46:11 +0900

    [Feat] 선택된 암유닛 강화 버튼 관련 텍스트 출력 기능
    Selectable로 선택된 암유닛 강화 버튼의 이름과 설명 텍스트를 출력하는 기능을 추가했습니다.

--- Assets/Workers/DEV/KYH/Scripts/UI/ArmUnitUpgrade.cs
+++ Assets/Workers/DEV/KYH/Scripts/UI/ArmUnitUpgrade.cs
@@ -1,24 +1,40 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 using Zenject;
 
 public class ArmUnitUpgrade : MonoBehaviour
 {
     [Inject] StatModel statModel;
 
+    [SerializeField] private Button button;
+
+    private void Start()
+    {
+        button = gameObject.GetComponent<Button>();
+    }
+
+    private void OnEnable()
+    {
+        button = gameObject.GetComponent<Button>();
+    }
+
     public void OnClickAttack_01Button()
     {
         statModel.SetAbility(AdditionAbility.AllPowerPer, 10);
+        button.interactable = false;
     }
 
     public void OnClickAttack_02Button()
     {
         statModel.SetAbility(AdditionAbility.AllPowerPer, 20);
+        button.interactable = false;
     }
 
     public void OnClickAttack_03Button()
     {
         statModel.SetAbility(AdditionAbility.AllPowerPer, 30);
+        button.interactable = false;
     }
 }

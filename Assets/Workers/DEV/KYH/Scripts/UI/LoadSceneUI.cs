440266ea5e796bda0592b21658a12c5c25c67ac3
KimYonai <msb0714@gmail.com>
2024-12-26 19:12:01 +0900

    [Feat] 저장된 게임 로드 기능 구현
    저장된 게임을 로드하는 기능을 추가했습니다.
    추후 버튼에 기능을 추가하여 테스트 예정입니다.

--- Assets/Workers/DEV/KYH/Scripts/UI/LoadSceneUI.cs
+++ Assets/Workers/DEV/KYH/Scripts/UI/LoadSceneUI.cs
@@ -9,14 +9,26 @@ using Zenject;
 public class LoadSceneUI : MonoBehaviour
 {
     [Inject] SaveManager saveManager;
+    [Inject] SaveData saveData;
+    [Inject] StatModel statModel;
 
-    private void Awake()
+    [ContextMenu("Load")]
+    public void Load()
     {
-        
+        if (!PlayerPrefs.HasKey("SaveData"))
+        {
+            Debug.LogWarning("저장된 데이터가 없습니다.");
+            return;
+        }
+
+        // PlayerPrefs에서 SaveData JSON 문자열 가져오기
+        string jsonData = PlayerPrefs.GetString("SaveData");
+        saveData = JsonUtility.FromJson<SaveData>(jsonData);
     }
 
-    private void Start()
+    public void OnClickLoadGameButton()
     {
-        
+        Load();
+        // TODO : 로비 씬으로 전환
     }
 }

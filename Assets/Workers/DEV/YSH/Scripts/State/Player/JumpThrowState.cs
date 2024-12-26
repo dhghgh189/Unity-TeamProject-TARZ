a0e890689a7144a15c8658c255a0c7c5f1fbcc25
dhghgh189 <dhghgh189@gmail.com>
2024-12-26 12:30:51 +0900

    [Feat] 점프 원거리 공격 구현
    - 점프 원거리 공격 스펙 필드 추가 (Interval) 및 인스펙터로 수치 설정
    - Jump, Fall State에서 현재 물건 스택이 점프 공격에 필요한 스택만큼 존재하는지 체크하여 진행되도록 함
    - JumpThrow 상태 진입 시 카메라 정면을 바라보도록 하고 코루틴을 통해 정해진 스펙대로 공격을 진행
    - PlayerAttack에 JumpThrow 작성

--- Assets/Workers/DEV/YSH/Scripts/State/Player/JumpThrowState.cs
+++ Assets/Workers/DEV/YSH/Scripts/State/Player/JumpThrowState.cs
@@ -4,6 +4,11 @@ using UnityEngine;
 
 public class JumpThrowState : BaseState<PlayerController>
 {
+    private Transform camTrf;
+    private Vector3 lookDir;
+
+    private Coroutine throwRoutine;
+
     public JumpThrowState(PlayerController owner)
     {
         this.owner = owner;
@@ -13,11 +18,45 @@ public class JumpThrowState : BaseState<PlayerController>
     public override void OnEnter()
     {
         base.OnEnter();
+        if (camTrf == null)
+            camTrf = Camera.main.transform;
+
+        // 카메라 정면을 바라본다.
+        lookDir = camTrf.forward;   // 공격 시전 시 바라봤던 방향을 기억해둔다.
+        owner.Movement.LookAt(lookDir);
+
+        owner.SkillHandler.Use(SkillEnum.ActTimingType.Attack);
+        throwRoutine = owner.StartCoroutine(ThrowRoutine());
+    }
+
+    IEnumerator ThrowRoutine()
+    {
+        for(int i = 0; i < owner.Attack.JumpThrowAmount; i++)
+        {
+            owner.Attack.JumpThrow();
+            yield return Util.GetDelay(owner.Attack.JumpThrowInterval);
+        }
     }
 
     public override void OnUpdate()
     {
         base.OnUpdate();
+
+        // 애니메이션 도중 회전이 원복되는 현상 방지 (Adjust)
+        if (lookDir != Vector3.zero && owner.transform.forward != lookDir)
+        {
+            owner.Movement.LookAt(lookDir);
+        }
+
+        if (owner.Movement.IsGrounded)
+        {
+            owner.StopCoroutine(throwRoutine);
+            throwRoutine = null;
+
+            owner.Movement.Move(Vector3.zero);
+            owner.ChangeState(EState.Idle);
+            return;
+        }
     }
 
     public override void OnFixedUpdate()

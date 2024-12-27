21584ba78d2ab3d5a1ea58962c9eafeb9f0b3aae
scv829 <scv829@naver.com>
2024-12-27 15:36:56 +0900

    Test: 마나스킬 2 임시 구현
    - 마나스킬 2 임시 구현
    - 추구 변경 가능성 있음

---
+++ Assets/Workers/DEV/HJS/Scripts/Skills/Mana/ThrowCarObject.cs
@@ -0,0 +1,55 @@
+using UnityEngine;
+using static UnityEngine.UI.GridLayoutGroup;
+
+public class ThrowCarObject : MonoBehaviour
+{
+    [Header("Init")]
+    [SerializeField] ParticleSystem effect;
+    [SerializeField] float speed;
+    [SerializeField] float hitDamage;
+    [SerializeField] float explosionDamage;
+    [SerializeField] float explosionRange;
+
+    [SerializeField] Rigidbody rigid;
+    private void Awake()
+    {
+        rigid = GetComponent<Rigidbody>();
+    }
+
+    public void Throw()
+    {
+        rigid.AddForce( (Vector3.right * 45f).normalized * speed, ForceMode.Impulse);
+    }
+
+    private void OnCollisionEnter(Collision other)
+    {
+        if(other.gameObject.layer.Equals(LayerMask.NameToLayer("Monster")))
+        {
+            IDamagable damagable = other.gameObject.GetComponent<IDamagable>();
+            if (damagable != null) { damagable.TakeDamage(hitDamage); }
+        }
+        else if(other.gameObject.layer.Equals(LayerMask.NameToLayer("Ground")))
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    private void OnDestroy()
+    {
+        Collider[] colliders = Physics.OverlapSphere(transform.position, explosionRange, LayerMask.GetMask("Monster"));
+        foreach (Collider collider in colliders)
+        {
+            IDamagable damagable = collider.gameObject.GetComponent<IDamagable>();
+            if (damagable != null) { damagable.TakeDamage(explosionDamage); Debug.Log($"{collider.gameObject.name}에게 {explosionDamage}만큼의 피해를 입혔다!"); }
+        }
+    }
+
+    public void Init(ManaSkillDataSO data)
+    {
+        speed = data.GetData((int)ManaThrowCarDataType.FlightSpeed);
+        hitDamage = data.GetData((int)ManaThrowCarDataType.HitDamage);
+        explosionDamage = data.GetData((int)ManaThrowCarDataType.ExplosionDamage);
+        explosionRange = data.GetData((int)ManaThrowCarDataType.ExplosionRange);
+    }
+
+}

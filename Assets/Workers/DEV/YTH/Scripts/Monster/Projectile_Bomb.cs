ff0d259e91f541ddfffc2edd9b085dea5e622a12
TaeHeon Ryoo <taroni55@naver.com>
2024-12-27 21:24:26 +0900

    [Feat] projectile 충돌 데미지, 독웅덩이 데미지 분리

--- Assets/Workers/DEV/YTH/Scripts/Monster/Projectile_Bomb.cs
+++ Assets/Workers/DEV/YTH/Scripts/Monster/Projectile_Bomb.cs
@@ -12,6 +12,7 @@ public class Projectile_Bomb : MonoBehaviour
     private void Start()
     {
         _monsterSkillManager = _bombZombie.GetComponent<MonsterSkillManager>();
+        _sphereCollider.radius = _monsterSkillManager.BombSkill.Range;
     }
 
     private void OnCollisionEnter(Collision collision)
@@ -19,16 +20,14 @@ public class Projectile_Bomb : MonoBehaviour
         StartCoroutine(CountDown());
         Debug.Log("바닥에 닿음");
     }
-
     private void OnTriggerEnter(Collider other)
     {
-        if (!other.transform.CompareTag("Player"))
-            return;
+        if (!other.transform.CompareTag("Player")) return;
 
+        Debug.Log("플레이어가 맞았다");
         IDamagable damagableObj = other.gameObject.GetComponent<IDamagable>();
         damagableObj.TakeDamage(_monsterSkillManager.BombSkill.Damage);
     }
-
     IEnumerator CountDown()
     {
         yield return Util.GetDelay(3f);

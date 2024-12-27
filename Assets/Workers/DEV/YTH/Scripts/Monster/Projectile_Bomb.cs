0305656fa1e01f91f8bd77f4c0d2f9170dd9867a
TaeHeon Ryoo <taroni55@naver.com>
2024-12-27 21:15:10 +0900

    [Refactor] Bomb 가시성 개선

--- Assets/Workers/DEV/YTH/Scripts/Monster/Projectile_Bomb.cs
+++ Assets/Workers/DEV/YTH/Scripts/Monster/Projectile_Bomb.cs
@@ -5,14 +5,13 @@ public class Projectile_Bomb : MonoBehaviour
 {
     [SerializeField] GameObject _bombZombie;
 
+    [SerializeField] SphereCollider _sphereCollider;
+
     private MonsterSkillManager _monsterSkillManager;
-    private SphereCollider _sphereCollider;
 
     private void Start()
     {
-        _sphereCollider = GetComponent<SphereCollider>();
         _monsterSkillManager = _bombZombie.GetComponent<MonsterSkillManager>();
-        _sphereCollider.radius = _monsterSkillManager.BombSkill.Range;
     }
 
     private void OnCollisionEnter(Collision collision)
@@ -20,19 +19,21 @@ public class Projectile_Bomb : MonoBehaviour
         StartCoroutine(CountDown());
         Debug.Log("바닥에 닿음");
     }
+
     private void OnTriggerEnter(Collider other)
     {
-        if (!other.transform.CompareTag("Player")) return;
+        if (!other.transform.CompareTag("Player"))
+            return;
 
-        Debug.Log("플레이어가 맞았다");
         IDamagable damagableObj = other.gameObject.GetComponent<IDamagable>();
         damagableObj.TakeDamage(_monsterSkillManager.BombSkill.Damage);
     }
+
     IEnumerator CountDown()
     {
-        yield return new WaitForSeconds(3f);
+        yield return Util.GetDelay(3f);
         _sphereCollider.enabled = true;
-        yield return new WaitForSeconds(0.1f);
+        yield return Util.GetDelay(0.1f);
         Destroy(gameObject);
     }
 }

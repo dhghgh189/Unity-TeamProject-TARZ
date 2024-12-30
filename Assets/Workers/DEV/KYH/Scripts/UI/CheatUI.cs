a30d3d5292b3f3e77e54ab3580a5d7c879790db0
KimYonai <msb0714@gmail.com>
2024-12-30 12:19:44 +0900

    [Feat] 칩 대량 지급 치트 기능 구현
    테스트용 칩 대량 지급 치트 기능을 구현했습니다.

--- Assets/Workers/DEV/KYH/Scripts/UI/CheatUI.cs
+++ Assets/Workers/DEV/KYH/Scripts/UI/CheatUI.cs
@@ -2,12 +2,21 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using Zenject;
 
 public class CheatUI : MonoBehaviour
 {
+    [Inject] StatModel model;
+
     public void MujeokMode(bool isOn)
     {
         Debug.Log(isOn);
         CheatManager.isMujeok = isOn;
+    }
+
+    public void ChipPlease()
+    {
+        model.Chip += 1000000f;
+        model.BlackChip += 1000000f;
     }
 }

b45bba599631abfb9cd48cd4f69009987a279245
scv829 <scv829@naver.com>
2024-12-30 09:12:38 +0900

    Feat: 마나 3스킬 구현
    마나 3스킬 구현

--- Assets/Workers/DEV/HJS/Scripts/Skills/Mana/MegaFistGateObject.cs
+++ Assets/Workers/DEV/HJS/Scripts/Skills/Mana/MegaFistGateObject.cs
@@ -1,23 +1,74 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Zenject.ReflectionBaking.Mono.Cecil;
 
 public class MegaFistGateObject : MonoBehaviour
 {
+    [Header("Fist_Init")]
     [SerializeField] MegaFistObject fist;
+    [SerializeField] Animator gateAnimator;
+    [Header("Fist_Data")]
+    [SerializeField] float fistAttackSpeed;
+    [SerializeField] float fistAttackRange;
+    [SerializeField] float fistReturnTime;
+
+
+    private int fistStartAnimHash;
+    private int fistReturnAnimHash;
+
+    private void Awake()
+    {
+        gateAnimator = GetComponent<Animator>();
+        fistStartAnimHash = Animator.StringToHash("FistStartAnimation");
+        fistReturnAnimHash = Animator.StringToHash("FistReturnAnimation");
+    }
 
     private void Start()
     {
+        StartCoroutine(FistStartRoutine());
     }
 
-    private IEnumerator tesRoutine()
+    // 시작할 때 애니메이션 실행
+    private IEnumerator FistStartRoutine()
     {
-        yield return new WaitForSeconds(1);
-        fist.MoveToward();
+        fist.Move();
+        gateAnimator.SetFloat("FistAttackSpeed", (fistAttackSpeed > 1) ? fistAttackSpeed : 1);
+        yield return Util.GetDelay(1f);
+        Debug.Log("시작!");
+        gateAnimator.CrossFade(fistStartAnimHash, 0.01f);
     }
 
+    public void OnStartAction()
+    {
+        fist.Return();
+        StartCoroutine(FistReturnRotine());
+    }
+
+    private IEnumerator FistReturnRotine()
+    {
+        yield return Util.GetDelay(fistReturnTime);
+        gateAnimator.CrossFade(fistReturnAnimHash, 0.01f);
+        
+    }
+
+    public void OnEndAction()
+    {
+        Destroy(gameObject);
+    }
+
+    // 데미지, 공격 범위, 주먹 속도, 주먹 시간 , 투명도
     public void Init(ManaSkillDataSO data)
     {
-        
+        // 데미지
+        fist.Damage = data.GetData((int)ManaMegaFistDataType.FistDamage);
+        // 공격 범위
+        transform.localScale = new Vector3( data.GetData((int)ManaMegaFistDataType.FistRange) * 2f, data.GetData((int)ManaMegaFistDataType.FistRange) * 2f, 0.07f);
+        transform.position += Vector3.up * data.GetData((int)ManaMegaFistDataType.FistRange) * 0.5f;
+        // 주먹 속도
+        fistAttackSpeed = data.GetData((int)ManaMegaFistDataType.FistSpeed);
+        // 주먹 시간
+        fistReturnTime = data.GetData((int)ManaMegaFistDataType.FistTime);
+        // TODO : 투명도
     }
 }

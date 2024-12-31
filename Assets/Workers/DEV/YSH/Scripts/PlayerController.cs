6b444ec36f24e17cb97c0a7a4e2d2b3aa4c7b579
dhghgh189 <dhghgh189@gmail.com>
2024-12-31 21:09:08 +0900

    [Feat] 상호작용 오브젝트 활성화, 비활성화 코드 추가

--- Assets/Workers/DEV/YSH/Scripts/PlayerController.cs
+++ Assets/Workers/DEV/YSH/Scripts/PlayerController.cs
@@ -30,6 +30,7 @@ public class PlayerController : MonoBehaviour, IDamagable
     [HideInInspector] public ManaSkillHandler ManaSkillHandler;
     [HideInInspector] public PlayerSkillHandler SkillHandler;
     [HideInInspector] public AblityAdapter AblityAdapter;
+    [HideInInspector] public Interactioner interactioner;
     public PlayerFSM Fsm { get; private set; }
     public Animator Anim { get { return anim; } }
     public PlayerInputHandler PInput { get; private set; }
@@ -39,6 +40,7 @@ public class PlayerController : MonoBehaviour, IDamagable
     public DrainManager Drain { get { return drainManager; } }
     public float delay { get; set; }
     public bool IsAnimStart { get; set; }
+    //public bool IsGrabingInput { get { return interactioner.IsGrabing; } }
 
     private StringBuilder sb;
     void Awake()
@@ -54,6 +56,7 @@ public class PlayerController : MonoBehaviour, IDamagable
         Attack = GetComponent<PlayerAttack>();
         SkillHandler = GetComponent<PlayerSkillHandler>();
         ManaSkillHandler = GetComponent<ManaSkillHandler>();
+        interactioner = GetComponentInChildren<Interactioner>();
 
         Fsm = new PlayerFSM(this, AblityAdapter);
 

bad98f0700a2dda4aed9a1fc175d37e4fc69badb
jiyeonUning <yeon14735@gmail.com>
2024-12-31 21:10:47 +0900

    [Feat] 특수 오브젝트 활성화 - 비활성화 기능 재수정 진행

--- Assets/Workers/DEV/IJY/Script/Example_Interaction_GrabScript.cs
+++ Assets/Workers/DEV/IJY/Script/Example_Interaction_GrabScript.cs
@@ -3,20 +3,19 @@ using UnityEngine;
 public class Example_Interaction_GrabScript : MonoBehaviour, Base_InteractionOBJ_Grab
 {
     public PlayerController playerController { get; set; }
-    [SerializeField] public bool GrabOnOff { get; set; }
-
-    void Start() => GrabOnOff = false;
 
     public void Activate_Grab()
     {
-        if (GrabOnOff == true)
-        {
-            Debug.Log($"{gameObject.name} : 활성화됨");
-        }
-        else if (GrabOnOff == false)
-        {
-            Debug.Log($"{gameObject.name} : 비활성화됨");
-        }
+        //if (playerController == null) return;
+
+        //if (playerController.IsGrabingInput == true)
+        //{
+        //    Debug.Log($"{gameObject.name} : 활성화됨");
+        //}
+        //else if (playerController.IsGrabingInput == false)
+        //{
+        //    Debug.Log($"{gameObject.name} : 비활성화됨");
+        //}
     }
 }
 

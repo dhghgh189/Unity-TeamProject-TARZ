af024471b64b23eea86e57e2fdef01802c26511e
februarywind <89524114+februarywind@users.noreply.github.com>
2025-01-01 18:28:27 +0900

    [Feat] 랜덤 맵 생성기 완성

--- Assets/Workers/DEV/YTH/Scripts/DungeonGenerator/MapGenerator.cs
+++ Assets/Workers/DEV/YTH/Scripts/DungeonGenerator/MapGenerator.cs
@@ -15,21 +15,21 @@ public class MapGenerator : MonoBehaviour
     private Vector3 destroyerY = new Vector3(0, 7f, 0);
 
     // 가장 먼 방을 찾기 위한 변수
-    Vector3 farDistancePos;
-    Vector3 bossRoomDir;
-
-    Vector3 maxZPos;
-    Vector3 maxXPos;
+    private Vector3 farDistancePos;
+    private Vector3 bossRoomDir;
+    private Vector3 maxZPos;
+    private Vector3 maxXPos;
 
     // 방을 생성할 위치
-    Vector3 createPos;
+    private Vector3 createPos;
 
     private int random;
 
-    void Start()
+    private void Start()
     {
         StartCoroutine(MapCreater());
     }
+
     IEnumerator MapCreater()
     {
         for (int i = 0; i < roomCount; i++)
@@ -44,15 +44,15 @@ public class MapGenerator : MonoBehaviour
                 continue;
             }
 
+            // 방 생성
             Instantiate(roomPrefab, createPos, Quaternion.identity, transform);
 
-            // 보스장 생성을 위한 가장 먼 방 체크
+            // 상점, 보스방 생성을 위한 가장 먼 방 체크
             FindFarRoomPos();
 
             // 랜덤 방향 지정
             random = Random.Range(0, 4);
             createPos += createDir[random] * 50f;
-
 
             // 진행 방향의 벽 제거
             if (i == roomCount - 1)
@@ -61,8 +61,16 @@ public class MapGenerator : MonoBehaviour
             wallDestroyer.transform.position = destroyerY + createPos - createDir[random] * 25f;
         }
 
+        // 보스방 방향 지정
         SetBossRoomDir();
-        Debug.Log(farDistancePos);
+
+        yield return Util.GetDelay(0.05f);
+
+        // 상점 생성
+        CreateStore();
+
+        yield return Util.GetDelay(0.05f);
+
         // 보스룸 통로 생성
         for (int i = 0; i < 2; i++)
         {
@@ -71,10 +79,12 @@ public class MapGenerator : MonoBehaviour
             wallDestroyer.transform.position = destroyerY + farDistancePos - bossRoomDir * 25f;
             farDistancePos += bossRoomDir * 50;
         }
+
         yield return Util.GetDelay(0.05f);
         Destroy(wallDestroyer.gameObject);
         Destroy(roomChecker.gameObject);
     }
+
     private void SetBossRoomDir()
     {
         while (true)
@@ -88,6 +98,7 @@ public class MapGenerator : MonoBehaviour
             farDistancePos -= bossRoomDir * 50;
         }
     }
+
     private void FindFarRoomPos()
     {
         if (farDistancePos.sqrMagnitude < createPos.sqrMagnitude)
@@ -102,10 +113,20 @@ public class MapGenerator : MonoBehaviour
 
     private void CreateStore()
     {
-        if (Vector3.Distance(farDistancePos, maxXPos) > Vector3.Distance(farDistancePos, maxXPos))
+        float setStoreDir = 0;
+        if (Vector3.Distance(farDistancePos, maxXPos) > Vector3.Distance(farDistancePos, maxZPos))
         {
-            //저
-            //밥좀?머ㅏㄱ고올겠여
+            setStoreDir = maxXPos.x < 0 ? -1 : 1;
+            maxXPos += Vector3.right * 50f * setStoreDir;
+            Instantiate(storePrefab, maxXPos, Quaternion.identity);
+            wallDestroyer.transform.position = destroyerY + maxXPos - Vector3.right * 25f * setStoreDir;
+        }
+        else
+        {
+            setStoreDir = maxZPos.z < 0 ? -1 : 1;
+            maxZPos += Vector3.forward * 50f * setStoreDir;
+            Instantiate(storePrefab, maxZPos, Quaternion.identity);
+            wallDestroyer.transform.position = destroyerY + maxZPos - Vector3.forward * 25f * setStoreDir;
         }
     }
 }

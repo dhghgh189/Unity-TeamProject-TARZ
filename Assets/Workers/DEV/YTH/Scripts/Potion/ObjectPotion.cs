b931483b9bc4f0da3c64b7581a800216795acf10
TaeHeon Ryoo <taroni55@naver.com>
2025-01-06 21:00:33 +0900

    [Feat] 오브젝트 포션 기능 구현 중

--- Assets/Workers/DEV/YTH/Scripts/Potion/ObjectPotion.cs
+++ Assets/Workers/DEV/YTH/Scripts/Potion/ObjectPotion.cs
@@ -1,18 +1,10 @@
 using UnityEngine;
 using Zenject;
 
-public class StaminaPotion : MonoBehaviour, IPotion
+public class ObjectPotion : MonoBehaviour, IPotion
 {
     [Inject]
     private PlayerController player;
-
-
-    [SerializeField] float value; // 스태미너 무한 유지 시간
-
-    public void Use()
-    {
-        player.InfStamina(value);
-    }
 
     private void OnTriggerEnter(Collider other)
     {
@@ -21,5 +13,10 @@ public class StaminaPotion : MonoBehaviour, IPotion
             Use();
             Destroy(gameObject);
         }
+    }
+
+    public void Use()
+    {
+       
     }
 }

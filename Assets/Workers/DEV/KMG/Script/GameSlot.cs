884a455d4043e78ebbd67014c92c346e3a280409
februarywind <89524114+februarywind@users.noreply.github.com>
2025-01-06 20:35:53 +0900

    [Fix] 코드 수정 및 주석 추가

--- Assets/Workers/DEV/KMG/Script/GameSlot.cs
+++ Assets/Workers/DEV/KMG/Script/GameSlot.cs
@@ -8,9 +8,11 @@ using Zenject;
 public class GameSlot : MonoBehaviour
 {
     [Inject] Loading LoadingObject;
-    [SerializeField] int slotIndex;
-    private SaveSlotData saveSlotData;
 
+    // 세이브 슬롯의 넘버링 고유한 번호를 가져야 함
+    [SerializeField] int slotIndex;
+
+    private SaveSlotData saveSlotData = new();
     private Button startButton;
     private Button resetButton;
 
@@ -20,31 +22,39 @@ public class GameSlot : MonoBehaviour
     {
         startButton = GetComponent<Button>();
         startButton.onClick.AddListener(GameStart);
+
         resetButton = GetComponentsInChildren<Button>()[1];
         resetButton.onClick.AddListener(ResetSlot);
     }
     private void Start()
     {
+        // 저장 경로에 파일이 있다면 역 직렬화를 통해 saveSlotData에 저장함
         path = Application.persistentDataPath + $"/SaveSlot{slotIndex}";
         if (!File.Exists(path))
         {
-            saveSlotData = new();
             startButton.GetComponentInChildren<TMP_Text>().text = "NewGame";
         }
         else
         {
             saveSlotData = JsonUtility.FromJson<SaveSlotData>(File.ReadAllText(path));
             startButton.GetComponentInChildren<TMP_Text>().text = new DateTime(saveSlotData.CreateTime).ToString();
-            Debug.Log(JsonUtility.ToJson(saveSlotData, true));
         }
+        // 세이브 매니저를 통한 저장을 위해 경로 저장
         saveSlotData.SlotPath = path;
     }
 
     private void GameStart()
     {
+        // 저장 파일이 없다면 생성
         if (!File.Exists(path))
+        {
             saveSlotData.CreateTime = DateTime.Now.Ticks;
+            File.WriteAllText(path, JsonUtility.ToJson(saveSlotData));
+        }
+
+        // saveSlotData을 프로젝트에 바인딩 
         FindAnyObjectByType<ProjectInstaller>().SaveSlotBind(saveSlotData);
+        
         LoadingObject.StartLoading(Define.SceneType.Lobby);
         gameObject.SetActive(false);
     }

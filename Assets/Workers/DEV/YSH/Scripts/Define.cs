d31c950268509ac8a21712c82c66a2fde929931a
dhghgh189 <dhghgh189@gmail.com>
2025-01-06 16:45:51 +0900

    [Feat] 일반 몬스터 반격 애니메이션 추가

--- Assets/Workers/DEV/YSH/Scripts/Define.cs
+++ Assets/Workers/DEV/YSH/Scripts/Define.cs
@@ -13,6 +13,7 @@ public static class Define
     public static readonly int HASH_ANIM_DRAIN = Animator.StringToHash("Drain");
     public static readonly int HASH_ANIM_DAMAGED = Animator.StringToHash("Damage");
     public static readonly int HASH_ANIM_DEAD = Animator.StringToHash("Die");
+    public static readonly int HASH_ANIM_COUNTER_THROW = Animator.StringToHash("CounterThrow");
 
     public const int USEKEY_MAXCOUNT = 4;
 

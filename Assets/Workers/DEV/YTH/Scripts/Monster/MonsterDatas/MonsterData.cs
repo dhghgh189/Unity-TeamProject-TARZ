ba527c58ce56063d302c750ce776077cf27f32d2
TaeHeon Ryoo <taroni55@naver.com>
2025-01-07 15:00:29 +0900

    [Feat] 몬스터 데이터 사망 여부 변수 생성

--- Assets/Workers/DEV/YTH/Scripts/Monster/MonsterDatas/MonsterData.cs
+++ Assets/Workers/DEV/YTH/Scripts/Monster/MonsterDatas/MonsterData.cs
@@ -37,6 +37,9 @@ public class MonsterData : MonoBehaviour
     [SerializeField] bool _isCatched; // 잡힌 상태 (이동 불가)
     public bool IsCatched { get { return _isCatched; } set { _isCatched = value; } }
 
+    [SerializeField] bool _isDead; // 사망 시
+    public bool IsDead { get { return _isDead;  }  set { _isDead = value; } }
+
     [Header("근거리 몬스터 평타")]
     [SerializeField] float _meleeAttackSpeed; // 근접 공격 속도
     public float MeleeAttackSpeed { get { return _meleeAttackSpeed; } set { _meleeAttackSpeed = value; } }

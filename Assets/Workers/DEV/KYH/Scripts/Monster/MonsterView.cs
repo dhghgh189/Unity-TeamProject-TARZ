b4e0dccbb826c9fa16abc000810824cbd0887fb8
YunHwan Kim <msb0714@gmail.com>
2025-01-09 12:39:21 +0900

    [Create] MonsterView 스크립트 생성
    엘리트/보스 몬스터의 체력 상태를 보여주기 위한 슬라이더 기능을 구현하기 위한 스크립트를 생성했습니다.

---
+++ Assets/Workers/DEV/KYH/Scripts/Monster/MonsterView.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using Zenject;
+
+public class MonsterView : MonoBehaviour
+{
+    [SerializeField] private Slider hpView;
+}

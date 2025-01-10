067c962f2c599941152426960973f4f567e121f5
jiyeonUning <yeon14735@gmail.com>
2025-01-10 15:09:39 +0900

    [Feat] 라인 렌더러 메테리얼 점선으로 변경

--- Assets/Workers/DEV/IJY/Script/Example_InteractionScript.cs
+++
@@ -1,9 +0,0 @@
-using UnityEngine;
-
-public class Example_InteractionScript : MonoBehaviour, Interaction_Ibase_Activate
-{
-    public void Activate()
-    {
-        Debug.Log($"{gameObject.name} : 말을 걸었다");
-    }
-}

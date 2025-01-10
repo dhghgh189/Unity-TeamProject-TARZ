d4a491687ced04eb32f3c253ffd871223877641e
jiyeonUning <yeon14735@gmail.com>
2025-01-10 20:32:16 +0900

    [Set] 돌발 퀘스트 NPC 구현 진행 예정

--- Assets/Workers/DEV/IJY/Script/_Interactioners/Reward_Interaction.cs
+++ Assets/Workers/DEV/IJY/Script/_Interactioners/Reward_Interaction.cs
@@ -1,7 +1,4 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
-using Zenject;
 
 public class Reward_Interaction : InteractionOBJ_Base, Interaction_Ibase_Activate
 {

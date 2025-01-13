e16fe062f5989296121b509ca6d955459f5ca260
scv829 <scv829@naver.com>
2025-01-13 09:09:07 +0900

    Feat: 가방 2스킬 추가
    가방 2스킬 추가

---
+++ Assets/Workers/DEV/HJS/Scripts/Skills/Bag/ScrapChildObject.cs
@@ -0,0 +1,29 @@
+using UnityEngine;
+
+public class ScrapChildObject : MonoBehaviour
+{
+    [SerializeField] ScrapParentObject parent;
+    [SerializeField] float range;
+    [SerializeField] ParticleSystem ps;
+    private void Awake()
+    {
+        ps = GetComponent<ParticleSystem>();
+    }
+
+    private void Start()
+    {
+        parent.OnStartEvent.AddListener(ps.Play);
+        parent.OnEndEvent.AddListener(ps.Stop);
+    }
+
+    private void OnParticleCollision(GameObject other)
+    {
+        MonsterData monsterData = other.GetComponent<MonsterData>();
+        if (other.GetComponent<MonsterData>() is null)
+        {
+            monsterData = other.GetComponentInParent<MonsterData>();
+        }
+
+        parent.ApplyEffect(monsterData);
+    }
+}

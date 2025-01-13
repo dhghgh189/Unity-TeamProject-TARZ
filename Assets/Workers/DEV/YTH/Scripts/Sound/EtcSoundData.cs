5e5efb54aaff2cb70a06b38dc550576923f3b7a4
TaeHeon Ryoo <taroni55@naver.com>
2025-01-13 12:39:12 +0900

    [Feat] 기타 사운드 프로퍼티 변수 생성

--- Assets/Workers/DEV/YTH/Scripts/Sound/EtcSoundData.cs
+++ Assets/Workers/DEV/YTH/Scripts/Sound/EtcSoundData.cs
@@ -6,20 +6,91 @@ using UnityEngine;
 public class EtcSoundData : ScriptableObject
 {
     [SerializeField] private Sound _sound;
-    public AudioClip M_TakeDamage { get { return _sound.M_TakeDamage; } }
+
+    /// <summary>
+    /// BGM
+    /// </summary>
+    public AudioClip TitleBGM { get { return _sound.TitleBGM; } }
+    public AudioClip LobbyBGM { get { return _sound.LobbyBGM; } }
+    public AudioClip FieldBGM { get { return _sound.FieldBGM; } }
+    public AudioClip GameOver { get { return _sound.GameOver; } }
+    public AudioClip GameClear { get { return _sound.GameClear; } }
+
+    /// <summary>
+    /// UI 사운드
+    /// </summary>
+    public AudioClip OnUI { get { return _sound.OnUI; } }
+    public AudioClip OffUI { get { return _sound.OffUI; } }
+    public AudioClip MoveUI { get { return _sound.MoveUI; } }
+    public AudioClip SelectUI { get { return _sound.SelectUI; } }
+    public AudioClip ApplyUI { get { return _sound.ApplyUI; } }
+
+    /// <summary>
+    /// 장비 사운드
+    /// </summary>
+    public AudioClip OnEquip { get { return _sound.OnEquip; } }
+    public AudioClip OffEquip { get { return _sound.OffEquip; } }
+    public AudioClip DecompositEquip { get { return _sound.DecompositEquip; } }
+
+    /// <summary>
+    /// 상점 사운드
+    /// </summary>
+    public AudioClip OnShop { get { return _sound.OnShop; } }
+    public AudioClip OffShop { get { return _sound.OffShop; } }
+    public AudioClip Buy { get { return _sound.Buy; } }
+    public AudioClip Reroll { get { return _sound.Reroll; } }
+
+    /// <summary>
+    /// Object
+    /// </summary>
+    public AudioClip GetBlueChip { get { return _sound.GetBlueChip; } }
+    public AudioClip GetRedChip { get { return _sound.GetRedChip; } }
+    public AudioClip GetEquipment { get { return _sound.GetEquipment; } }
+    public AudioClip GetMoney { get { return _sound.GetMoney; } }
+    public AudioClip HPBuff { get { return _sound.HPBuff; } }
+    public AudioClip StaminaBuff { get { return _sound.StaminaBuff; } }
+    public AudioClip ManaBuff { get { return _sound.ManaBuff; } }
+    public AudioClip ObejectBuff { get { return _sound.ObejectBuff; } }
+
     
-
-
 
     [Serializable]
     public struct Sound
     {
         [Header("BGM")]
-        public AudioClip lobbyBGM;
+        public AudioClip TitleBGM;
+        public AudioClip LobbyBGM;
+        public AudioClip FieldBGM;
+        public AudioClip GameOver;
+        public AudioClip GameClear;
 
         [Header("SFX")]
-        public AudioClip M_TakeDamage;
+        [Header("UI")]
+        public AudioClip OnUI;               // UI 켜기
+        public AudioClip OffUI;              // UI 닫기
+        public AudioClip MoveUI;             // UI 이동
+        public AudioClip SelectUI;           // UI 선택
+        public AudioClip ApplyUI;            // UI 적용
 
-       
+        [Header("Equipment")]
+        public AudioClip OnEquip;            // 장착
+        public AudioClip OffEquip;           // 분리
+        public AudioClip DecompositEquip;    // 분해
+
+        [Header("Shop")]
+        public AudioClip OnShop;             // 상점 켜기
+        public AudioClip OffShop;            // 상점 닫기
+        public AudioClip Buy;                // 구매
+        public AudioClip Reroll;             // 리롤
+
+        [Header("Object")]
+        public AudioClip GetBlueChip;
+        public AudioClip GetRedChip;
+        public AudioClip GetEquipment;
+        public AudioClip GetMoney;
+        public AudioClip HPBuff;
+        public AudioClip StaminaBuff;
+        public AudioClip ManaBuff;
+        public AudioClip ObejectBuff;
     }
 }

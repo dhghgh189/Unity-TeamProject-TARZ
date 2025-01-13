f871a803ae2bf015697a48fb24a166ef510a98b8
scv829 <scv829@naver.com>
2025-01-13 13:59:12 +0900

    Refactor: 그래비티 샷 로직 수정
    그래비티 샷 로직 수정

--- Assets/Workers/DEV/HJS/Scripts/Skills/Functions/GravityShoot.cs
+++ Assets/Workers/DEV/HJS/Scripts/Skills/Functions/GravityShoot.cs
@@ -1,5 +1,8 @@
 using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
+using static MonsterData;
 
 /// <summary>
 /// 중력장을 발생하는 스크립트
@@ -9,6 +12,10 @@ public class GravityShoot : MonoBehaviour, ISpec
     [SerializeField] float operationTime;   // 동작하는 시간
     [SerializeField] float force;           // 끌어당기는 힘
     [SerializeField] float range;           // 끌어당기는 범위
+
+    private Coroutine coroutine;
+    [SerializeField] Rigidbody rigid;
+    [SerializeField] List<GameObject> enemies;
 
     private void Init()
     {
@@ -21,25 +28,30 @@ public class GravityShoot : MonoBehaviour, ISpec
         // 끌어당기기
         if (other.gameObject.layer.Equals(LayerMask.NameToLayer("Monster")))
         {
-            Debug.Log("StartHolidng");
-            StartCoroutine(StartBoilingRoutine(other.gameObject.transform));
+            MonsterData data = (other.gameObject.GetComponent<MonsterData>() is not null) ? other.gameObject.GetComponent<MonsterData>() : other.gameObject.GetComponentInParent<MonsterData>();
+            if (data.MonsterTIer.Equals(MonsterTier.Normal))
+            {
+                enemies.Add(data.gameObject);
+                StartCoroutine(StartBoilingRoutine(data.gameObject.transform));
+            }
         }
     }
-
     private IEnumerator StartBoilingRoutine(Transform other)
     {
+        /* NavMeshAgent -> Rigidbody 물리(강체) 적용하기 위한 행동 */
+        yield return null;
+        SetHold(other.gameObject);
+        Debug.Log("초기설정");
+
+        Debug.Log("물리 적용");
         while (true)
         {
-            // 거리를 계산하고
+            /* 물리 적용 */
             Vector3 relativeDirection = other.position - transform.position;
-            // 거리가 중력장 크기보다 크면 중지
-            if (relativeDirection.sqrMagnitude > range * range) yield break;
-
-            // 아니면 정규화를 진행하고
             Vector3 gravityDirection = relativeDirection.normalized;
 
-            // 현재 있는 오브젝트 방향으로 힘의 량만큼 끌어당기기
-            other.gameObject.GetComponent<Rigidbody>().velocity = -gravityDirection * force;
+            rigid.AddExplosionForce(force * -256f * Time.deltaTime, transform.position, range);
+            /* 해당 적용이 다 끝나는 조건 */
             yield return new WaitForFixedUpdate();
         }
     }
@@ -47,6 +59,11 @@ public class GravityShoot : MonoBehaviour, ISpec
     // 중력장 지속시간이 다 되어서 파괴되는 경우
     private void OnDestroy()
     {
+        foreach (var enemy in enemies)
+        {
+            if (enemy is null) continue;
+            SetPut(enemy);
+        }
         // 모든 코루틴 종료
         StopAllCoroutines();
     }
@@ -58,5 +75,37 @@ public class GravityShoot : MonoBehaviour, ISpec
         range = spec.Range(level);
 
         Init();
+    }
+
+    /// <summary>
+    /// 잡는 함수 -> 물리(강체)를 사용하기 위한 설정
+    /// </summary>
+    /// <param name="other">사용 요청을 한 물체</param>
+    private void SetHold(GameObject other)
+    {
+        NavMeshAgent agent = other.GetComponent<NavMeshAgent>();
+        Rigidbody rigid = other.GetComponent<Rigidbody>();
+        MonsterData monsterData = other.GetComponent<MonsterData>();
+        monsterData.IsCatched = true;
+        agent.enabled = false;
+        rigid.useGravity = true;
+        rigid.isKinematic = false;
+    }
+
+    /// <summary>
+    /// 놓아주는 함수 -> NavMesh를 활성화하기 위한 설정
+    /// </summary>
+    /// <param name="other"></param>
+    private void SetPut(GameObject other)
+    {
+        /* Rigidbody -> NavMeshAgent 다시 navMesh를 활성화하기 위한 행동 */
+        NavMeshAgent agent = other.GetComponent<NavMeshAgent>();
+        Rigidbody rigid = other.GetComponent<Rigidbody>();
+        MonsterData monsterData = other.GetComponent<MonsterData>();
+        agent.enabled = true;
+        rigid.useGravity = false;
+        rigid.isKinematic = true;
+        monsterData.IsCatched = false;
+        Debug.Log("원복 끝");
     }
 }

86dd759136a0b30c359f40007ee146c60e53b3c9
februarywind <89524114+februarywind@users.noreply.github.com>
2025-01-15 17:08:03 +0900

    [Fix] 상점 사운드 예외처리

--- Assets/Workers/DEV/KMG/Script/StoreSound.cs
+++ Assets/Workers/DEV/KMG/Script/StoreSound.cs
@@ -5,13 +5,15 @@ public class StoreSound : MonoBehaviour
     AudioClip tempClip;
     private void OnTriggerEnter(Collider other)
     {
-        if (SoundManager.BGM.clip == SoundManager.SoundData_UI.StoreBGM)
+        if (!other.CompareTag("Player")|| SoundManager.BGM.clip == SoundManager.SoundData_UI.StoreBGM)
             return;
         tempClip = SoundManager.BGM.clip;
         SoundManager.PlayBGM(SoundManager.SoundData_UI.StoreBGM);
     }
     private void OnTriggerExit(Collider other)
     {
+        if (!other.CompareTag("Player"))
+            return;
         SoundManager.PlayBGM(tempClip);
     }
 }

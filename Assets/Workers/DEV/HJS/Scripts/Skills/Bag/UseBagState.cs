6c6013382588ffabf3d512a4e06cfc314ccb3193
scv829 <scv829@naver.com>
2025-01-17 16:32:26 +0900

    Fix: 상태에서 못나오는 상황 예외처리
    상태에서 못나오는 상황 예외처리

--- Assets/Workers/DEV/HJS/Scripts/Skills/Bag/UseBagState.cs
+++ Assets/Workers/DEV/HJS/Scripts/Skills/Bag/UseBagState.cs
@@ -9,6 +9,7 @@ public class UseBagState : BaseState<PlayerController>
 {
     // 해당 능력의 사용이  끝났는지 확인하는 변수
     public bool IsEnd { get => owner.BagSkillHandler.ActionEnd; }
+    private float excpetionTimer;
 
     public UseBagState(PlayerController owner)
     {
@@ -17,6 +18,7 @@ public class UseBagState : BaseState<PlayerController>
 
     public override void OnEnter()
     {
+        excpetionTimer = 0f;
         base.OnEnter();
         owner.BagSkillHandler.CurNode.Value.OnEnter();
     }
@@ -24,6 +26,14 @@ public class UseBagState : BaseState<PlayerController>
     public override void OnUpdate()
     {
         base.OnUpdate();
+
+        excpetionTimer += Time.deltaTime;
+
+        if (excpetionTimer >= 5f)
+        {
+            owner.ChangeState(EState.Idle);
+            return;
+        }
 
         if (owner.PInput.TryDash && owner.IsEnoughStamina(owner.Stat.DashStaminaAmount))
         {
